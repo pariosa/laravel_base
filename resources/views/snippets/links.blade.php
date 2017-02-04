acdb68a37ef5fdcf6e291eeb2fb9a07c987c54aa
pariosa <ariosa@gmail.com>
2017-02-04 02:20:13 -0500

    test
    test

---
+++ resources/views/snippets/links.blade.php
@@ -0,0 +1,6 @@
+                <div class="links">
+                    <a href="/">main</a>
+                    <a href="/home">home</a>
+                    <a href="/chat">chat</a> 
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
