32070528e7b8032ed0867eb0a29cdbe599c2fb33
ariosap <ariosa@gmail.com>
2017-02-23 16:20:42 -0500

    modal

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -43,13 +43,33 @@
 				Disabilities: <span class="disabilities-{{$contact->id}}">{{$contact->disabilities}}</span>
 				</div>
 				<div class="col-sm-1">
-				<form method="POST" role="form" action="/contact/delete">
-				{!! csrf_field() !!}
-				<input class="hidden" type='text' name='id' value="{{$contact->id}}"/>
-				<button class="btn btn-danger btn-xs btn-primary" value="submit">Delete</button>
+				
+<div class="modal fade" id="delete-{{$contact->id}}">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Confirm</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Do you really want to delete {{$contact->name}}?</p>
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn- btn-small" data-dismiss="modal">Cancel</button>
+   	<button type="button" class="btn btn-danger btn-small btn-primary" onclick="submit({{$contact->id}})"  data-dismiss="modal" value="submit" >Delete</button>
 
-				</form>
-								<button class="btn btn-primary btn-sm btn-warning" onclick="updateForm({{$contact->id}})">&nbsp;Edit&nbsp;</button>
+
+	 </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+								<button class="btn btn-danger btn-xs btn-primary" data-toggle="modal" data-target="#delete-{{$contact->id}}">Delete</button>
+								<button class=" edit-btn btn btn-primary btn-sm btn-warning" onclick="updateForm({{$contact->id}})">&nbsp;Edit&nbsp;</button>
 
 				</div>
 
@@ -62,73 +82,17 @@
 	<div class="form-container panel-default col-md-12">
 
 	</div>
+	@if($contacts)
 	{{$contacts->links()}}
+	@endif
 	<div class="panel-default col-md-12">
 
-	<button class="add btn btn-primary btn-sm" style="margin-bottom:15px;" onclick="appendform()">Add Contact +</button>
+	<button class="add add-btn btn btn-primary btn-sm" style="margin-bottom:15px;" onclick="appendform()">Add Contact +</button>
 	</div>
 
 	</div>
 
-	<script>
-	function appendform(){
-		var form = '';
-		form += '<form action="/contact/create" role="form" method="POST">'
-		form += '{!! csrf_field() !!}';
-		form += '<div class="form-group">'
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="name" placeholder="name"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="nickname" placeholder="nickname"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="phone" placeholder="Phone Number"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="email" placeholder="Email"/></div>';
-		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="job" placeholder="Job"/></div>';
-		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="gender" placeholder="gender"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="disabilities" placeholder="disabilities"/></div>';
-		form += '<input type="text" class="hidden" name="owner" value="{{auth()->user()->id}}"/>';
-		form += '<button class="btn btn-primary btn-success btn-sm" value="submit">Create Contact</button>';
-		form += '</div>'
-		form += '</form>';
-
-		$('.form-container').append(form);
-	}
-
-	function updateForm(id){
-		console.log($('.name-'+id).val());
-		var form = '';
-		form += '<form action="/contact/update" role="form" method="POST">'
-		form += '{!! csrf_field() !!}';
-		form += '<input name="id" class="hidden" value="'+id+'" />';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm"  name="name" placeholder="name" value="'+$('.name-'+id).text()+'"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="nickname" placeholder="nickname" value="'+$('.nickname-'+id).text()+'"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="phone" placeholder="Phone Number" value="'+$('.phone-'+id).text()+'"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="email" placeholder="Email" value="'+$('.email-'+id).text()+'"/></div>';
-		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="job" placeholder="Job" value="'+$('.job-'+id).text()+'"/></div>';
-		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="gender" placeholder="gender" value="'+$('.gender-'+id).text()+'"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="disabilities" placeholder="disabilities" value="'+$('.disabilities-'+id).text()+'"/></div>';
-		form += '<input type="text" class="hidden" name="owner" value="1"/>';
-		form += '<button class="btn btn-primary btn-sm btn-success" value="submit">update Contact</button>';
-		form += '</form>';
-
-		$('.contact-'+id).append(form);
-	}
-	function searchForm(){
-		var form = '';
-		form += '<form action="/contact/search" role="form" method="POST">'
-		form += '{!! csrf_field() !!}';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="name" placeholder="name"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="nickname" placeholder="nickname"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="phone" placeholder="Phone Number"/></div>';
-		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="email" placeholder="Email"/></div>';
-		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="job" placeholder="Job"/></div>';
-		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="gender" placeholder="gender"/></div>';
-		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="disabilities" placeholder="disabilities"/></div>';
-		form += '<input type="text" class="hidden" name="owner" value="{{auth()->user()->id}}"/>';
-		form += '<button class="btn btn-info btn-sm" value="submit">Search!</button>';
-		form += '</form>';
-
-		$(".search").append(form);
-		$(".search-button").hide();
-	}
-	</script>
+@include('snippets.rolodex')
 </div>
 
 @else
---
+++ resources/views/snippets/rolodex.blade.php
@@ -0,0 +1,73 @@
+	<script>
+	function appendform(){
+		var form = '';
+		form += '<form action="/contact/create" role="form" method="POST">'
+		form += '{!! csrf_field() !!}';
+		form += '<div class="form-group">'
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="name" placeholder="name"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="nickname" placeholder="nickname"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="phone" placeholder="Phone Number"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="email" placeholder="Email"/></div>';
+		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="job" placeholder="Job"/></div>';
+		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="gender" placeholder="gender"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="disabilities" placeholder="disabilities"/></div>';
+		form += '<input type="text" class="hidden" name="owner" value="{{auth()->user()->id}}"/>';
+		form += '<button class="btn btn-primary btn-success btn-sm" value="submit">Create Contact</button>';
+		form += '</div>'
+		form += '</form>';
+
+		$('.form-container').append(form);
+
+		$(".add-btn").hide();
+	}
+
+	function updateForm(id){
+		console.log($('.name-'+id).val());
+		var form = '';
+		form += '<form action="/contact/update" role="form" method="POST">'
+		form += '{!! csrf_field() !!}';
+		form += '<input name="id" class="hidden" value="'+id+'" />';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm"  name="name" placeholder="name" value="'+$('.name-'+id).text()+'"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="nickname" placeholder="nickname" value="'+$('.nickname-'+id).text()+'"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="phone" placeholder="Phone Number" value="'+$('.phone-'+id).text()+'"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="email" placeholder="Email" value="'+$('.email-'+id).text()+'"/></div>';
+		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="job" placeholder="Job" value="'+$('.job-'+id).text()+'"/></div>';
+		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="gender" placeholder="gender" value="'+$('.gender-'+id).text()+'"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="disabilities" placeholder="disabilities" value="'+$('.disabilities-'+id).text()+'"/></div>';
+		form += '<input type="text" class="hidden" name="owner" value="1"/>';
+		form += '<button class="btn btn-primary btn-sm btn-success" value="submit">update Contact</button>';
+		form += '</form>';
+
+		$('.contact-'+id).append(form);
+
+		$(".edit-btn").hide();
+	}
+	function searchForm(){
+		var form = '';
+		form += '<form action="/contact/search" role="form" method="POST">'
+		form += '{!! csrf_field() !!}';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="name" placeholder="name"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="nickname" placeholder="nickname"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="phone" placeholder="Phone Number"/></div>';
+		form += '<div class="col-md-3"><input type="text" class="form-control input-sm" name="email" placeholder="Email"/></div>';
+		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="job" placeholder="Job"/></div>';
+		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="gender" placeholder="gender"/></div>';
+		form += '<div class="col-md-2"><input type="text" class="form-control input-sm" name="disabilities" placeholder="disabilities"/></div>';
+		form += '<input type="text" class="hidden" name="owner" value="{{auth()->user()->id}}"/>';
+		form += '<button class="btn btn-info btn-sm" value="submit">Search!</button>';
+		form += '</form>';
+
+		$(".search").append(form);
+		$(".search-button").hide();
+	}
+
+function submit(id){
+ form= "";
+ form += '<form method="POST" id="delete-'+id+'" role="form" action="/contact/delete">';
+ form += '{!! csrf_field() !!}';
+ form +='<input class="hidden" type="text" name="id" value="'+id+'"/>';
+ form +='</form>';
+ $(form).appendTo('body').submit();
+}
+
+	</script>
