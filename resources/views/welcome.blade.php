f97055ad0ea10c6ee46c38e54cd9fef58a48cfe0
pariosa <ariosa@gmail.com>
2017-05-14 14:52:49 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Callxander
+                    CallxEnder
                 </div>
 
             @include('snippets.links')
